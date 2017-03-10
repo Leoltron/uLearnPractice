2c60da5e83ba7d5a92d130eeb2e9727a2c1633c4
Leoltron <leoole5@gmail.com>
2017-03-10 11:36:16 +0500

    c

---
+++ Seminar/DictTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Seminar
+{
+    [TestFixture]
+    public class DictTests
+    {
+        [Test]
+        public void PerfomanceTest()
+        {
+            var cap = 10000;
+            var dict = new Dictionary<int,int>(cap);
+
+        } 
+    }
+}
