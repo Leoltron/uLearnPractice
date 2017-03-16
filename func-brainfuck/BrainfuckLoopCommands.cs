c7ce8a75a87060b866f3ba46e12aad6cbab9a812
Leoltron <leoole5@gmail.com>
2017-03-16 16:47:40 +0500

    Refactoring

--- func-brainfuck/BrainfuckLoopCommands.cs
+++ func-brainfuck/BrainfuckLoopCommands.cs
@@ -5,14 +5,49 @@ namespace func.brainfuck
 
     public class BrainfuckLoopCommands
     {
-        private static Dictionary<int, int> loopStartEndDict;
-        private static Dictionary<int, int> loopEndStartDict;
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var loops = new LoopBorders(vm);
 
-        private static void RegisterLoops(IVirtualMachine vm)
+            vm.RegisterCommand('[', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.InstructionPointer = loops.GetEnd(b.InstructionPointer);
+            });
+            vm.RegisterCommand(']', b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                    b.InstructionPointer = loops.GetStart(b.InstructionPointer);
+            });
+        }
+
+    }
+
+    class LoopBorders
+    {
+        private Dictionary<int, int> loopStartEndDict;
+        private Dictionary<int, int> loopEndStartDict;
+
+        public int GetEnd(int start)
+        {
+            return loopStartEndDict[start];
+        }
+
+        public int GetStart(int end)
+        {
+            return loopEndStartDict[end];
+        }
+
+        public LoopBorders(IVirtualMachine vm)
         {
             loopStartEndDict = new Dictionary<int, int>();
             loopEndStartDict = new Dictionary<int, int>();
 
+            RegisterLoops(vm);
+        }
+
+        private void RegisterLoops(IVirtualMachine vm)
+        {
             var loopStartStack = new Stack<int>();
             for (var i = 0; i < vm.Instructions.Length; i++)
             {
@@ -31,22 +66,6 @@ namespace func.brainfuck
                 }
             }
         }
-
-        public static void RegisterTo(IVirtualMachine vm)
-        {
-            RegisterLoops(vm);
-            vm.RegisterCommand('[', b =>
-            {
-                if (b.Memory[b.MemoryPointer] == 0)
-                    b.InstructionPointer = loopStartEndDict[b.InstructionPointer];
-            });
-            vm.RegisterCommand(']', b =>
-            {
-                if (b.Memory[b.MemoryPointer] != 0)
-                    b.InstructionPointer = loopEndStartDict[b.InstructionPointer];
-            });
-        }
-
     }
 
 }
