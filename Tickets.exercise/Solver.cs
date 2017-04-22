c9ee77642004216fdb96bccf404f059b04d5053f
Leoltron <leoole5@gmail.com>
2017-04-22 23:45:08 +0500

    Solved "Tickets"

--- Tickets.exercise/Solver.cs
+++ Tickets.exercise/Solver.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using System.Numerics;
 
 namespace Tickets
 {
     public static class Solver
     {
-        public static string Solve(int totalLen, int totalSum)
+        public static BigInteger Solve(int totalLen, int totalSum)
         {
-            throw new NotImplementedException();
+            if (totalSum % 2 != 0)
+                return 0;
+            totalSum /= 2;
+            var t = new BigInteger[totalLen + 1, totalSum + 1];
+            t[0, 0] = 1;
+            for (var sum = 0; sum <= totalSum; sum++)
+            for (var len = 1; len <= totalLen; len++)
+            {
+                t[len, sum] = 0;
+                for (var i = 0; i <= 9 && sum - i >= 0; i++)
+                    t[len, sum] += t[len - 1, sum - i];
+            }
+            var solve = t[totalLen, totalSum];
+            return solve * solve;
         }
     }
 }
