5989e799b4e5594a609dd6ea12f1739a6f4d9098
Leoltron <leoole5@gmail.com>
2017-04-22 19:10:37 +0500

    Solved "Flags"

--- Flags.exercise/Solver.cs
+++ Flags.exercise/Solver.cs
@@ -1,12 +1,16 @@
-﻿using System;
-
-namespace Flags
+﻿namespace Flags
 {
     public static class Solver
     {
         public static long Solve(int n)
         {
-            throw new NotImplementedException();
+            if (n <= 2) return 2;
+            var results = new long[n];
+            results[0] = 2;
+            results[1] = 2;
+            for (var i = 2; i < n; i++)
+                results[i] = results[i - 1] + results[i - 2];
+            return results[n - 1];
         }
     }
 }
