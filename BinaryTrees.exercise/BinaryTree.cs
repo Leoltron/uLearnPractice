e688517f5f15944f615e923ae34779bc20e79014
Leoltron <leoole5@gmail.com>
2017-04-28 20:39:00 +0500

    Added BinaryTrees Task

---
+++ BinaryTrees.exercise/BinaryTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        
+    }
+
+    class TreeLeave<T> where T : IComparable
+    {
+        
+    }
+}
