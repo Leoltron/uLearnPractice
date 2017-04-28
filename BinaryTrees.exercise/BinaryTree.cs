401e4caf5cc5ab052f78f1338b9111e91c6edae1
Leoltron <leoole5@gmail.com>
2017-04-28 21:42:02 +0500

    Task 1 kinda finished.

--- BinaryTrees.exercise/BinaryTree.cs
+++ BinaryTrees.exercise/BinaryTree.cs
@@ -1,18 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinaryTrees
 {
     public class BinaryTree<T> where T : IComparable
     {
-        
+        private TreeNode<T> root;
+
+        public void Add(T key)
+        {
+            var nodeToAdd = new TreeNode<T>(key);
+            if (root == null)
+            {
+                root = nodeToAdd;
+                return;
+            }
+            var currentNode = root;
+            while(true)
+            {
+                if (currentNode.Value.CompareTo(key) > 0)
+                {
+                    if (currentNode.Left == null)
+                    {
+                        currentNode.Left = nodeToAdd;
+                        break;
+                    }
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if (currentNode.Right == null)
+                    {
+                        currentNode.Right = nodeToAdd;
+                        break;
+                    }
+                    currentNode = currentNode.Right;
+                }
+            }
+        }
+
+        public bool Contains(T key)
+        {
+            var currentNode = root;
+            while (currentNode != null)
+            {
+                var diff = currentNode.Value.CompareTo(key);
+                if (diff == 0)
+                    return true;
+                currentNode = diff > 0 ? currentNode.Left : currentNode.Right;
+            }
+            return false;
+        }
     }
 
-    class TreeLeave<T> where T : IComparable
+    class TreeNode<T> where T : IComparable
     {
-        
+        public T Value { get; private set; }
+        public TreeNode<T> Left, Right;
+
+        public TreeNode(T value)
+        {
+            if(value == null)
+                throw new ArgumentNullException(nameof(value)); 
+            Value = value;
+        }
     }
 }
