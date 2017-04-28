c56bece657a1d1f1893eb668588d8c56ebc4c14c
Leoltron <leoole5@gmail.com>
2017-04-28 11:01:04 +0500

    Refactoring

--- DiskTree.exercise/Solver.cs
+++ DiskTree.exercise/Solver.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace DiskTree
 {
@@ -9,42 +8,18 @@ namespace DiskTree
     {
         public static IEnumerable<string> Solve(IEnumerable<string> input)
         {
-            return input.OrderBy(s => s, new Comparer())
-                .Select(line => line.Split('\\')).ToList().GetDirTree();
+            return input.Select(line => line.Split('\\')).ToList().GetDirTree();
         }
 
         private static IEnumerable<string> GetDirTree(this List<string[]> paths, int depth = 0)
         {
             if (depth > 0)
-                yield return paths.First()[depth - 1].AddSpacesToLeft(depth - 1);
+                yield return new string(' ', depth - 1) + paths.First()[depth - 1];
             foreach (var treePart in paths.Where(path => path.Length > depth)
                 .GroupBy(path => path[depth])
-                .OrderBy(group => group.Key, new Comparer())
+                .OrderBy(group => group.Key, StringComparer.Ordinal)
                 .SelectMany(group => group.ToList().GetDirTree(depth + 1)))
                 yield return treePart;
-        }
-
-        private static string AddSpacesToLeft(this string s, int spacesAmount)
-        {
-            if (spacesAmount < 0) throw new ArgumentOutOfRangeException(nameof(spacesAmount));
-            if (spacesAmount == 0)
-                return s;
-            var sb = new StringBuilder();
-            for (var i = 0; i < spacesAmount; i++)
-                sb.Append(' ');
-            sb.Append(s);
-            return sb.ToString();
-        }
-
-        private class Comparer : IComparer<string>
-        {
-            public int Compare(string x, string y)
-            {
-                if (x == y) return 0;
-                if (x.StartsWith(y)) return 1;
-                if (y.StartsWith(x)) return -1;
-                return string.CompareOrdinal(x,y);
-            }
         }
     }
 }
